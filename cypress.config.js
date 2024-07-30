bde8b3a1380c4bc8ddcaf1cd5bafce04f348a5bc
Thabit Ibrahim <thabit252@gmail.com>
2024-07-30 12:17:55 +0800

    hi

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,9 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true,
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      return config;
+      // implement node event listeners here
     },
   },
 });
