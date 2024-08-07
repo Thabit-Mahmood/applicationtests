cfc72a91cc6ab5e9005e32bfbb3ad84ffc0ad081
Thabit Ibrahim <thabit252@gmail.com>
2024-08-07 15:48:02 +0800

    all pass

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,6 @@ module.exports = defineConfig({
     json: true,
   },
   video: true,
-  screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
