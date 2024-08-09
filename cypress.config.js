c9040458d12444a59cb49516abde134fbd80e094
Thabit Ibrahim <thabit252@gmail.com>
2024-08-09 10:23:46 +0800

    minor modification

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,13 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-multi-reporters',
   reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true,
+    configFile: 'reporter-config.json',
   },
   video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     },
     videoUploadOnPasses: false,
