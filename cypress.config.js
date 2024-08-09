da9fae685da59238be8c15298acc94f35baf8210
Thabit Ibrahim <thabit252@gmail.com>
2024-08-09 10:43:30 +0800

    minor modification

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,28 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  reporter: 'cypress-multi-reporters',
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    configFile: 'reporter-config.json',  // Use a config file for multiple reporters
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
   },
   video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);  // Integrate mochawesome reporter
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+      // Add JUnit reporter
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // if the video has failed tests, delete it
+          if (results.stats.failures === 0) {
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
+
       return config;
     },
     videoUploadOnPasses: false,
