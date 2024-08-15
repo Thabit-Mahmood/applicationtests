06092d9db574518ca0b40c77451b0de5ec515eed
Thabit Ibrahim <thabit252@gmail.com>
2024-08-16 07:53:29 +0800

    this

--- cypress/e2e/ApplicationTests.cy.js
+++ cypress/e2e/ApplicationTests.cy.js
@@ -3,6 +3,6 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
   it('test 2', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://wrong-example.cypress.io')
   })
 })
