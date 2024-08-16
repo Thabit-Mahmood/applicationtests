bb602199ac70280ce1c137b84f25651191f96038
Thabit Ibrahim <thabit252@gmail.com>
2024-08-16 11:17:33 +0800

    reverse

--- cypress.config.js
+++ cypress.config.js
@@ -11,12 +11,12 @@ module.exports = defineConfig({
   video: true,
   e2e: {
     setupNodeEvents(on, config) {
+      // Load the Cypress Mochawesome Reporter plugin
       require('cypress-mochawesome-reporter/plugin')(on);
 
       // Load the Testomatio plugin
       require('@testomatio/reporter/lib/adapter/cypress-plugin')(on, config);
       
-      // Load the Cypress Mochawesome Reporter plugin
       
       return config;
     },
