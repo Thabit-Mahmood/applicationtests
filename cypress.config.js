1bc056062e8166ef264b56dd2aa3460291220fce
Thabit Ibrahim <thabit252@gmail.com>
2024-08-16 10:38:10 +0800

    s

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,23 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // testomat.io reporter plugin:
-      require('@testomatio/reporter/lib/adapter/cypress-plugin')(on, config);
+      // Load the Testomatio plugin
+      
+      // Load the Cypress Mochawesome Reporter plugin
+      require('cypress-mochawesome-reporter/plugin')(on);
+      
+      return config;
     },
+    videoUploadOnPasses: false,
   },
-})
+});
