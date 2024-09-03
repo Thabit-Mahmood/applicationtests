c63a112d1a383ed3e21fbef7c402b46b236e2d2f
Thabit Ibrahim <thabit252@gmail.com>
2024-09-03 12:24:17 +0800

    parallel setup

---
+++ cypress/e2e/Admin.cy.js
@@ -0,0 +1,17 @@
+describe('Admin Test', () => {
+  it('visits example.cypress.io', () => {
+    cy.visit('https://www.youtube.com/');
+  });
+  after(() => {
+    const specName = Cypress.spec.name;
+    const results = {
+      totalTests: Cypress.mocha.getRunner().suite.tests.length,
+      numPassedTests: Cypress.mocha.getRunner().stats.passes,
+      numFailedTests: Cypress.mocha.getRunner().stats.failures,
+      numPendingTests: Cypress.mocha.getRunner().stats.pending,
+    };
+  
+    cy.saveResults(specName, results);
+  });
+  
+});
